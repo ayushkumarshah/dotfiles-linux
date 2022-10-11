323f71837ad301171b865dfb6b1d1f969ca2d635
Ayush Kumar Shah <as1211@rjb.cs.rit.edu>
2022-10-10 23:42:25 -0400

    Correct nvim dependencies

--- nvim_dependencies.sh
+++ nvim_dependencies.sh
@@ -7,7 +7,7 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
-sudo dpkg -i ripgrep_11.0.2_amd64.deb\n
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
 
 # Node js for neovim
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
