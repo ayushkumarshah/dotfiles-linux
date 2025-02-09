575503b408b917d4bde9fc28985d756e9f3552a5
ayushkumarshah <ayush.kumar.shah@gmail.com>
2025-02-09 14:50:34 -0500

    Changes from master

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,7 +1,7 @@
 alias python="python3"
 # alias vim="~/Downloads/nvim.appimage"
 # alias nvim="~/Downloads/nvim.appimage"
-alisas vim="nvim"
+alias vim="nvim"
 alias o="nautilus ."
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
